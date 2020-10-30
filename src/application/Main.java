13b88706d278995a191b2177be7038cbd0a763e7
GuilhermeDellatin <guidellatin@gmail.com>
2020-10-30 15:06:42 -0300

    Classe departamento

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
 
+import model.entities.Department;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Department department = new Department(1, "Books");
+        System.out.println(department);
     }
 }
